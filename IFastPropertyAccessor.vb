3047a2d195a417eabc9f3c06832a456d88f4b474
Quiles <jejquiles@gmail.com>
2014-10-13 16:59:34 +0200

    Interfaces

---
+++ IFastPropertyAccessor.vb
@@ -0,0 +1,29 @@
+Imports System.Collections.Generic
+Imports System.Reflection
+Imports Infosante.Elisagenda.Infrastructure.Common
+
+Public Interface IFastPropertyAccessor
+
+    ''' <summary>
+    ''' Gets the properties.
+    ''' </summary>
+    ''' <value>
+    ''' The properties.
+    ''' </value>
+    ReadOnly Property Properties As PropertyInfo()
+    ''' <summary>
+    ''' Gets the getter map.
+    ''' </summary>
+    ''' <value>
+    ''' The getter map.
+    ''' </value>
+    ReadOnly Property GetterMap As IDictionary(Of PropertyInfo, PropertyGetterDelegate)
+    ''' <summary>
+    ''' Gets the setter map.
+    ''' </summary>
+    ''' <value>
+    ''' The setter map.
+    ''' </value>
+    ReadOnly Property SetterMap As IDictionary(Of PropertyInfo, PropertySetterDelegate)
+
+End Interface
---
+++ IFastPropertyProvider.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Collections.Generic
+Imports System.Reflection
+Imports Infosante.Elisagenda.Infrastructure.Common
+
+Public Interface IFastPropertyProvider
+
+    ''' <summary>
+    ''' Stores the specified type.
+    ''' </summary>
+    ''' <param name="type">The type.</param>
+    Sub Store(type As Type)
+
+    ''' <summary>
+    ''' Gets the property information.
+    ''' </summary>
+    ''' <param name="type">The type.</param>
+    ''' <returns></returns>
+    Function GetPropertyInfo(type As Type) As PropertyInfo()
+    ''' <summary>
+    ''' Gets the property getter.
+    ''' </summary>
+    ''' <param name="type">The type.</param>
+    ''' <returns></returns>
+    Function GetPropertyGetter(type As Type) As IDictionary(Of PropertyInfo, PropertyGetterDelegate)
+    ''' <summary>
+    ''' Gets the property setter.
+    ''' </summary>
+    ''' <param name="type">The type.</param>
+    ''' <returns></returns>
+    Function GetPropertySetter(type As Type) As IDictionary(Of PropertyInfo, PropertySetterDelegate)
+
+    ''' <summary>
+    ''' Gets the item.
+    ''' </summary>
+    ''' <value>
+    ''' The item.
+    ''' </value>
+    Default ReadOnly Property Item(type As Type) As IFastPropertyAccessor
+    ''' <summary>
+    ''' Gets the type cached.
+    ''' </summary>
+    ''' <value>
+    ''' The type cached.
+    ''' </value>
+    ReadOnly Property TypeCached As IEnumerable(Of Type)
+
+End Interface
